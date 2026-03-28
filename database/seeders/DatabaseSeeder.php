f87d61a03371e46703762e200d6d91e1a528db0d
paufrancisco <francisco.paulo.pf@gmail.com>
2026-03-28 22:35:26 +0800

    setting up databases

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::create([
+            'name'     => 'Admin User',
+            'email'    => 'admin@inventory.com',
+            'password' => bcrypt('password'),
+            'role'     => 'admin',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name'     => 'Cashier User',
+            'email'    => 'cashier@inventory.com',
+            'password' => bcrypt('password'),
+            'role'     => 'cashier',
         ]);
     }
 }
